1e0ca2d4d1b4a6fe61620bcb94fb0052b503386a
vijay yadav <vy9369238330@gmail.com>
2026-03-24 15:48:31 +0530

    next route

--- index.mjs
+++ index.mjs
@@ -5,6 +5,9 @@ const port = 5000
 server.get("/", (req, res) => {
     res.json({ name: "apple"})
 })
+server.get("/hello", (req, res) => {
+    res.json({ name: "banana"})
+})
 
 server.listen(port, () =>{
     console.log(`server started on ${port}`)
