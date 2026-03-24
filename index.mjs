ff80ccfbde0c47f7df85ab0582f4a9d63d2b6802
vijay yadav <vy9369238330@gmail.com>
2026-03-24 16:59:59 +0530

    controller

--- index.mjs
+++ index.mjs
@@ -2,12 +2,14 @@ import express from 'express'
 const server = express()
 const port = 5000
 
-server.get("/", (req, res) => {
-    res.json({ name: "apple"})
-})
-server.get("/hello", (req, res) => {
-    res.json({ name: "banana"})
-})
+server.use(express.json())
+
+server.post("/login", (req, res) => {
+    console.log(req.body)
+    res.status(200).json({
+        message: "success"
+    })
+    })
 
 server.listen(port, () =>{
     console.log(`server started on ${port}`)
